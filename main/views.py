d23cdfdd6e79c4c131dc4a7582bf3b27359420ff
Rasheev <rashif.aunur@ui.ac.id>
2023-12-06 14:43:54 +0700

    Tutorial 2 - 1

--- main/views.py
+++ main/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from main.forms import ProductForm
+from main.models import Product
 
 # Create your views here.
 def show_main(request):
+    products = Product.objects.all()
+
     context = {
         'name': 'Sped Ran',
         'class': 'PBP S',
+        'product': products,
     }
 
     return render(request, "main.html", context)
+
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+    
+    context = {'form': form}
+    return render(request, "create_product.html", context)
