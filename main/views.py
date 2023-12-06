5dce28c7193811394cd0028e03954c6ccaa838ab
Rasheev <rashif.aunur@ui.ac.id>
2023-12-06 14:10:21 +0700

    Tutorial 1

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Sped Ran',
+        'class': 'PBP S',
+    }
+
+    return render(request, "main.html", context)
